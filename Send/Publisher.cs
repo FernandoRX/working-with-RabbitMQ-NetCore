553d2a379eaac5fe5f0bc08c07a64ea47d4c2963
FernandoRX <fernandonaylrx@gmail.com>
2018-04-11 22:55:18 -0300

    Creating Publisher and Consumer

---
+++ Send/Publisher.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using RabbitMQ.Client;
+using System.Text;
+
+namespace Send
+{
+    public class Publisher
+    {
+        public static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: "DeverDeCasa",
+                                 durable: false,
+                                 exclusive: false,
+                                 autoDelete: false,
+                                 arguments: null);
+
+                    string message = "Estudar RabbitMQ!";
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "",
+                                         routingKey: "DeverDeCasa",
+                                         basicProperties: null,
+                                         body: body);
+
+                    Console.WriteLine("Mensagem enviada: "+ message);
+                }
+
+            Console.WriteLine("Presisone [Enter] para sair");
+            Console.ReadLine();
+                
+            }
+        }
+    }
+}
